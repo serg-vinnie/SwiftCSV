32237499dace77d478575efebc0d32e2adb2724a
Sergiy Vynnychenko <sergiy@vynnychenko.com>
2020-05-30 00:13:39 +0300

    compile fix

--- SwiftCSV/String+Lines.swift
+++ SwiftCSV/String+Lines.swift
@@ -9,11 +9,40 @@
 extension String {
     var firstLine: String {
         var index = startIndex
-        let chars = characters
-        while index < endIndex && chars[index] != "\r\n" && chars[index] != "\n" && chars[index] != "\r" {
+        while index < endIndex && self[index] != "\r\n" && self[index] != "\n" && self[index] != "\r" {
             index = self.index(index, offsetBy: 1)
         }
         
-        return self.substring(to: index)
+        return String(self[startIndex..<index])
+    }
+}
+
+private extension String {
+    subscript (i: Int) -> String {
+        get {
+            return self[i ..< i + 1]
+        }
+        set (value) {
+            let range = index(startIndex, offsetBy: i) ..< index(startIndex, offsetBy: i+1)
+            self.replaceSubrange(range, with : value)
+        }
+    }
+    
+    subscript (r: Range<Int>) -> String {
+        get {
+            let range = Range(uncheckedBounds: (lower: max(0, min(count, r.lowerBound)),
+                                                upper: min(count, max(0, r.upperBound))))
+            let start = index(startIndex, offsetBy: range.lowerBound)
+            let end = index(start, offsetBy: range.upperBound - range.lowerBound)
+            return String(self[start ..< end])
+        }
+        set (value) {
+            let range = Range(uncheckedBounds: (lower: max(0, min(count, r.lowerBound)),
+                                                upper: min(count, max(0, r.upperBound))))
+            let start = index(startIndex, offsetBy: range.lowerBound)
+            let end = index(start, offsetBy: range.upperBound - range.lowerBound)
+            
+            self.replaceSubrange(start ..< end, with : value)
+        }
     }
 }
